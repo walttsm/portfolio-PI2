526180f7fd5bcce1c4c4e46a777ef21aa9c15b6c
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-03-24 14:52:39 -0300

    Conteúdo página projects adicionado

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import ProjectsStyles from "./projects.module.scss"
 
 const ProjectsPage = () => {
   return (
     <Layout>
       <h1>Projetos</h1>
-      <p>Em breve, listaremos nossos projetos aqui.</p>
+      <div className={ProjectsStyles.project}>
+        <h2>Este blog</h2>
+        <p>
+          Criamos este blog, para maiores informações, clique{" "}
+          <Link to="/" className={ProjectsStyles.link}>
+            aqui
+          </Link>
+          .
+        </p>
+      </div>
+      <hr />
+      <div className={ProjectsStyles.project}>
+        <h2>Jogo simples</h2>
+        <p>
+          Estamos em estágios iniciais de criação deste projeto para outra
+          disciplina. Fique de olho por mais atualizações!
+        </p>
+      </div>
     </Layout>
   )
 }
