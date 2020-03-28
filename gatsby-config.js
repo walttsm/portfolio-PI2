dcd4c54d8ae8d4c1d8b3dcb89ff09e3184595da7
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-03-28 20:08:56 -0300

    Conteudo walter em quem somos adicionado

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+
     "gatsby-plugin-sass",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,82 @@
 import React from "react"
-import { Link } from "gatsby"
+import { FaGithub, FaInstagram, FaLinkedin, FaSteam } from "react-icons/lib/fa"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import aboutStyles from "./about.module.scss"
+import walterImg from "../assets/images/walter.jpg"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Us" />
     <h1>Quem somos</h1>
-    <div>
-      <img alt="Foto de Erick" />
-      <h2>Erick Hoga</h2>
-      <p>O conteúdo do erick.</p>
+    <div className={aboutStyles.container}>
+      <div className={aboutStyles.imgSocial}>
+        <img className={aboutStyles.img} alt="Foto de Erick" src="" />
+        <div className={aboutStyles.social}>
+          <a href="https://www.instagram.com/walttsm/">
+            <FaInstagram />
+          </a>
+          <a href="https://github.com/walttsm">
+            <FaGithub />
+          </a>
+          <a href="https://steamcommunity.com/id/walttsm">
+            <FaSteam />
+          </a>
+          <a href="https://www.linkedin.com/in/walter-marinho-47a98b190/">
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+      <div className={aboutStyles.info}>
+        <div className={aboutStyles.infoText}>
+          <p>Coloca o texto aqui erick</p>
+        </div>
+      </div>
     </div>
     <hr />
-    <div>
-      <img alt="Foto de Walter" />
-      <h2>Walter Marinho</h2>
-      <p>O conteúdo do walter.</p>
+    <div className={aboutStyles.container}>
+      <div className={aboutStyles.imgSocial}>
+        <img className={aboutStyles.img} alt="Foto de Walter" src={walterImg} />
+        <div className={aboutStyles.social}>
+          <a href="https://www.instagram.com/walttsm/">
+            <FaInstagram />
+          </a>
+          <a href="https://github.com/walttsm">
+            <FaGithub />
+          </a>
+          <a href="https://steamcommunity.com/id/walttsm">
+            <FaSteam />
+          </a>
+          <a href="https://www.linkedin.com/in/walter-marinho-47a98b190/">
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+
+      <div className={aboutStyles.info}>
+        <h2>Walter Marinho</h2>
+        <div className={aboutStyles.infoText}>
+          <p>Olá, meu nome é Walter e vou te falar um pouco sobre mim.</p>
+          <p>
+            Nasci em Apucarana-PR, e atualmente moro em Londrina-PR, e estudo
+            Ciência da computação na UNIFIL. Atualmente tenho feito alguns
+            projetos em diversas linguagens, como java, python e javascript.
+            Estou me direcionando para o estudo de React e React Native.
+          </p>
+          <p>Alguns de meus Hobbies são:</p>
+          <ol>
+            <li>Jogos</li>
+            <li>Fotografia</li>
+            <li> Esportes</li>
+          </ol>
+          <p>
+            Caso quiser, fique à vontade para me seguir nas redes sociais. Vamos
+            interagir!
+          </p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
