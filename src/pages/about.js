7ed3401febc3a496d896466e0098b2581ecafa11
Erick Hoga <erickhoga@edu.unifil.br>
2020-03-29 19:10:08 -0300

    Adicionados links ,items e conteudos

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import aboutStyles from "./about.module.scss"
+import erickImg from "../assets/images/erick.png"
 import walterImg from "../assets/images/walter.jpg"
 
 const AboutPage = () => (
@@ -13,25 +14,31 @@ const AboutPage = () => (
     <h1>Quem somos</h1>
     <div className={aboutStyles.container}>
       <div className={aboutStyles.imgSocial}>
-        <img className={aboutStyles.img} alt="Foto de Erick" src="" />
+        <img className={aboutStyles.img} alt="Foto de Erick" src={erickImg} />
         <div className={aboutStyles.social}>
-          <a href="https://www.instagram.com/walttsm/">
+          <a href="/about">
             <FaInstagram />
           </a>
-          <a href="https://github.com/walttsm">
+          <a href="https://github.com/erickhoga">
             <FaGithub />
           </a>
-          <a href="https://steamcommunity.com/id/walttsm">
+          <a href="https://steamcommunity.com/id/erickhoga">
             <FaSteam />
           </a>
-          <a href="https://www.linkedin.com/in/walter-marinho-47a98b190/">
+          <a href="https://www.linkedin.com/in/erick-hoga-46597a156/">
             <FaLinkedin />
           </a>
         </div>
       </div>
       <div className={aboutStyles.info}>
+          <h2>Erick Hoga</h2>
         <div className={aboutStyles.infoText}>
-          <p>Coloca o texto aqui erick</p>
+          <p>Olá, tenho 22 anos atualmente estudante no instituto Unifil em ciência da computação, hoje moro em Londrina(PR) porém já morei 9 anos no Japão e tenho intenção de trabalhar/estudar lá na área de desenvolvimento de jogos.
+            <br></br><br></br>
+            Fiz alguns cursos entre eles: <br/> <br/>
+            - Técnico de informática (Senai); <br/> <br/>
+            - Curso de japonês (Vicente Rijo); <br/> <br/>
+          </p>
         </div>
       </div>
     </div>
