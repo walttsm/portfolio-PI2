f70a67a6dd51d1cecbfa4de111759de92b434205
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-05-31 19:06:48 -0300

    Estilização dos projetos

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,7 +7,8 @@ import ProjectsStyles from "./projects.module.scss"
 import SEO from "../components/seo"
 import img1 from "../images/projects/img1.jpg"
 import img2 from "../images/projects/img2.jpg"
-
+import pc from "../images/projects/pc.jpg"
+import setup from "../images/projects/setup.jpg"
 
 const ProjectsPage = () => {
   return (
@@ -18,7 +19,7 @@ const ProjectsPage = () => {
         <h2>Este blog</h2>
         <p>
           Criamos este blog, para maiores informações, clique{" "}
-          <Link to="/" className={ProjectsStyles.link}>
+          <Link to="/blog" className={ProjectsStyles.link}>
             aqui
           </Link>
           .
@@ -28,16 +29,17 @@ const ProjectsPage = () => {
       <div className={ProjectsStyles.project}>
         <h2>Animador de algoritmos</h2>
         <p>
-          Para estudar os algoritmos de classificação e ordenação,
-           fizemos um programa em JAVA que anima cada etapa de ordenação 
-           para alguns algoritmos. Abaixo fica um link para o repositório GitLab do projeto.
+          Para estudar os algoritmos de classificação e ordenação, fizemos um
+          programa em JAVA que anima cada etapa de ordenação para alguns
+          algoritmos. Abaixo fica um link para o repositório GitLab do projeto.
         </p>
 
-        <p>Link para repositório:
+        <p>
+          Link para repositório:
           <a href="https://gitlab.com/walttsm/animador-de-classificadores">
-          <FaGitlab />
+            <FaGitlab />
           </a>
-        </p>  
+        </p>
       </div>
       <hr />
       <div className={ProjectsStyles.project}>
@@ -53,56 +55,81 @@ const ProjectsPage = () => {
       <div className={ProjectsStyles.project}>
         <h2>Jogo em Unity</h2>
         <p>
-          Um jogo que consiste em uma batalha contra o computador utilizando da mecanica do jo ken po, em formas de objeto que se colidem.<br/><br/>
-          Ainda não muito desenvolvido porém com escolhas de personagens e fases e poderes através de combo.<br/><br/>
-          <div>
-            <img src={img1} alt="foto1" width="600" height="300"/> 
-            <img src={img2} alt="foto2" width="600" height="300"/> 
+          Um jogo que consiste em uma batalha contra o computador utilizando da
+          mecanica do jo ken po, em formas de objeto que se colidem.
+          <br />
+          <br />
+          Ainda não muito desenvolvido porém com escolhas de personagens e fases
+          e poderes através de combo.
+          <br />
+          <br />
+          <div className={ProjectsStyles.erickImages}>
+            <img src={img1} alt="foto1" width="600" height="300" />
+            <img src={img2} alt="foto2" width="600" height="300" />
           </div>
-          <br/><br/>
+          <br />
+          <br />
           <p>
-            Pode ser feito o download através do link_    
-            <a href="https://drive.google.com/drive/u/0/folders/1UEzZQTKZDh_A8Rl6uN-6iKfN1OoP4tM0">Download</a>
+            Pode ser feito o download através do link_
+            <a href="https://drive.google.com/drive/u/0/folders/1UEzZQTKZDh_A8Rl6uN-6iKfN1OoP4tM0">
+              Download
+            </a>
           </p>
         </p>
       </div>
       <hr />
       <h2>Walter</h2>
-        <div className={ProjectsStyles.project}>
-          <h2>Setup</h2>
-          <p>
-            Montei meu setup por conta própria, onde comprei as peças para o desktop e o montei.
-            Segue mais detalhes da configuração:<br/><br/>
-            <ul>
-              <li>Placa-mãe ASUS Prime b350-plus</li>
-              <li>Processador Ryzen 1600x six-core @ 3.8 Ghz</li>
-              <li>16 Gb Ram DDR4 3000 Mhz</li>
-              <li>Placa de vídeo ASUS GTX1080</li>
-              <li>Fonte EVGA 750W 80 plus bronze</li>
-              <li>Monitor Samsung 144Hz 1ms</li>
-            </ul>
-            Além disso, utilizo um Notebook DELL Inspiron 5537 para trabalhos que exigem portabilidade.
-            <img src="imagem do setup" alt="Computador"/> 
-            <img src="imagem do pc" alt="Computador"/> 
-            </p>
-          </div>
-      <hr />
       <div className={ProjectsStyles.project}>
-          <h2>Sistema de ponto PHP</h2>
-          <p>
-            Usando PHP em conjunto com a biblioteca XAMPP, desenvolvi um pequeno sistema de pontos,
-             com o objetivo de me familiarizar com relações com bancos de dados e familiaridade 
-             com queries de consulta em bancos.
-          </p>
-          <p>Link para repositório: <a href="https://github.com/walttsm/sistema-ponto-php"><FaGithub/></a></p>
+        <h2>Setup</h2>
+        <p>
+          Montei meu setup por conta própria, onde comprei as peças para o
+          desktop e o montei. Segue mais detalhes da configuração:
+        </p>
+        <ul>
+          <li>Placa-mãe ASUS Prime b350-plus</li>
+          <li>Processador Ryzen 1600x six-core @ 3.8 Ghz</li>
+          <li>16 Gb Ram DDR4 3000 Mhz</li>
+          <li>Placa de vídeo ASUS GTX1080</li>
+          <li>Fonte EVGA 750W 80 plus bronze</li>
+          <li>Monitor Samsung 144Hz 1ms</li>
+        </ul>
+        <p>
+          Além disso, utilizo um Notebook DELL Inspiron 5537 para trabalhos que
+          exigem portabilidade.
+        </p>
+        <div className={ProjectsStyles.images}>
+          <img src={setup} alt="Setup" width="350" />
+          <img src={pc} alt="Computador" width="350" />
+        </div>
       </div>
       <hr />
       <div className={ProjectsStyles.project}>
-          <h2>Gerador de declarações</h2>
-          <p>
-            Usando python em conjunto com ReportLab, criei um gerador de declarações para bancas de TCC de forma automatizada.
-          </p>
-          <p>Link para repositório: <a href="https://gitlab.com/npi-unifil/gerador-de-declara-es"><FaGitlab/></a></p>
+        <h2>Sistema de ponto PHP</h2>
+        <p>
+          Usando PHP em conjunto com a biblioteca XAMPP, desenvolvi um pequeno
+          sistema de pontos, com o objetivo de me familiarizar com relações com
+          bancos de dados e familiaridade com queries de consulta em bancos.
+        </p>
+        <p>
+          Link para repositório:{" "}
+          <a href="https://github.com/walttsm/sistema-ponto-php">
+            <FaGithub />
+          </a>
+        </p>
+      </div>
+      <hr />
+      <div className={ProjectsStyles.project}>
+        <h2>Gerador de declarações</h2>
+        <p>
+          Usando python em conjunto com ReportLab, criei um gerador de
+          declarações para bancas de TCC de forma automatizada.
+        </p>
+        <p>
+          Link para repositório:{" "}
+          <a href="https://gitlab.com/npi-unifil/gerador-de-declara-es">
+            <FaGitlab />
+          </a>
+        </p>
       </div>
       <hr />
     </Layout>
